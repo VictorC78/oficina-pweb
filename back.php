0d8e2a27fdca4391a499d0620819a93c00250fdf
VictorC78 <victorcarlos0000000000@gmail.com>
2023-11-11 18:18:47 -0300

    ...

---
+++ back.php
@@ -0,0 +1,7 @@
+<div class="nav-item active">
+    <a class="nav-link" href="oficina.php">
+        <i class="fas fa-fw fa-home text-white"></i>
+        <span class="text-white">Oficina</span></a>
+</div>
+
+
